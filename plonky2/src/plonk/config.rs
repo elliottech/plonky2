45d75ece09997ce51b64e7c23ff79496157ee96d
lighter-zz <allaboutshop8@163.com>
2025-11-12 16:32:45 -0500

    clippy

--- plonky2/src/plonk/config.rs
+++ plonky2/src/plonk/config.rs
@@ -52,7 +52,7 @@ pub trait Hasher<F: RichField>: Sized + Copy + Debug + Eq + PartialEq {
     fn hash_pad(input: &[F]) -> Self::Hash {
         let mut padded_input = input.to_vec();
         padded_input.push(F::ONE);
-        while (padded_input.len() + 1) % Self::Permutation::RATE != 0 {
+        while !(padded_input.len() + 1).is_multiple_of(Self::Permutation::RATE) {
             padded_input.push(F::ZERO);
         }
         padded_input.push(F::ONE);
