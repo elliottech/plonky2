19c0d3ed895cb7202cb9c634893ccf0c63f08fc2
lighter-zz <allaboutshop8@163.com>
2025-12-09 12:39:05 -0500

    Update hash.rs

--- plonky2/src/hash/poseidon2/hash.rs
+++ plonky2/src/hash/poseidon2/hash.rs
@@ -368,7 +368,7 @@ impl Poseidon2 for F {
             .iter_mut()
             .zip(EXTERNAL_CONSTANTS[external_round].iter())
             .for_each(|(x, &m)| {
-                *x = x.add_canonical_u64(m);
+                *x = unsafe { x.add_canonical_u64(m) };
             });
     }
 
