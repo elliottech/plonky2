a38fabb46ffd6b805be823984a7936f8532c6668
lighter-zz <allaboutshop8@163.com>
2026-02-23 12:01:05 -0500

    fix recursion

--- plonky2/src/hash/merkle_proofs.rs
+++ plonky2/src/hash/merkle_proofs.rs
@@ -151,25 +151,12 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     ) {
         debug_assert!(H::AlgebraicPermutation::RATE >= NUM_HASH_OUT_ELTS);
 
-        let zero = self.zero();
         let mut state: HashOutTarget = self.hash_or_noop::<H>(leaf_data);
         debug_assert_eq!(state.elements.len(), NUM_HASH_OUT_ELTS);
 
         for (&bit, &sibling) in leaf_index_bits.iter().zip(&proof.siblings) {
             debug_assert_eq!(sibling.elements.len(), NUM_HASH_OUT_ELTS);
-
-            let mut perm_inputs = H::AlgebraicPermutation::default();
-            perm_inputs.set_from_slice(&state.elements, 0);
-            perm_inputs.set_from_slice(&sibling.elements, NUM_HASH_OUT_ELTS);
-            // Ensure the rest of the state, if any, is zero:
-            perm_inputs.set_from_iter(core::iter::repeat(zero), 2 * NUM_HASH_OUT_ELTS);
-            let perm_outs = self.permute_swapped::<H>(perm_inputs, bit);
-            let hash_outs = perm_outs.squeeze()[0..NUM_HASH_OUT_ELTS]
-                .try_into()
-                .unwrap();
-            state = HashOutTarget {
-                elements: hash_outs,
-            };
+            state = H::two_to_one_swapped(state, sibling, bit, self);
         }
 
         for i in 0..NUM_HASH_OUT_ELTS {
@@ -196,7 +183,6 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     ) {
         debug_assert!(H::AlgebraicPermutation::RATE >= NUM_HASH_OUT_ELTS);
 
-        let zero = self.zero();
         let mut state: HashOutTarget = self.hash_or_noop::<H>(leaf_data);
         debug_assert_eq!(state.elements.len(), NUM_HASH_OUT_ELTS);
 
@@ -205,19 +191,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
 
         for (&bit, &sibling) in leaf_index_bits.iter().zip(&proof.siblings) {
             debug_assert_eq!(sibling.elements.len(), NUM_HASH_OUT_ELTS);
-
-            let mut perm_inputs = H::AlgebraicPermutation::default();
-            perm_inputs.set_from_slice(&state.elements, 0);
-            perm_inputs.set_from_slice(&sibling.elements, NUM_HASH_OUT_ELTS);
-            // Ensure the rest of the state, if any, is zero:
-            perm_inputs.set_from_iter(core::iter::repeat(zero), 2 * NUM_HASH_OUT_ELTS);
-            let perm_outs = self.permute_swapped::<H>(perm_inputs, bit);
-            let hash_outs = perm_outs.squeeze()[0..NUM_HASH_OUT_ELTS]
-                .try_into()
-                .unwrap();
-            state = HashOutTarget {
-                elements: hash_outs,
-            };
+            state = H::two_to_one_swapped(state, sibling, bit, self);
             // Store state at specific indices
             for n in 0..num_log_n - 1 {
                 final_states[n] = final_states[n + 1];
@@ -251,7 +225,6 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
     ) {
         debug_assert!(H::AlgebraicPermutation::RATE >= NUM_HASH_OUT_ELTS);
 
-        let zero = self.zero();
         let mut state: HashOutTarget = self.hash_or_noop::<H>(leaf_data[0].clone());
         debug_assert_eq!(state.elements.len(), NUM_HASH_OUT_ELTS);
 
@@ -259,19 +232,7 @@ impl<F: RichField + Extendable<D>, const D: usize> CircuitBuilder<F, D> {
         let mut leaf_data_index = 1;
         for (&bit, &sibling) in leaf_index_bits.iter().zip(&proof.siblings) {
             debug_assert_eq!(sibling.elements.len(), NUM_HASH_OUT_ELTS);
-
-            let mut perm_inputs = H::AlgebraicPermutation::default();
-            perm_inputs.set_from_slice(&state.elements, 0);
-            perm_inputs.set_from_slice(&sibling.elements, NUM_HASH_OUT_ELTS);
-            // Ensure the rest of the state, if any, is zero:
-            perm_inputs.set_from_iter(core::iter::repeat(zero), 2 * NUM_HASH_OUT_ELTS);
-            let perm_outs = self.permute_swapped::<H>(perm_inputs, bit);
-            let hash_outs = perm_outs.squeeze()[0..NUM_HASH_OUT_ELTS]
-                .try_into()
-                .unwrap();
-            state = HashOutTarget {
-                elements: hash_outs,
-            };
+            state = H::two_to_one_swapped(state, sibling, bit, self);
             current_height -= 1;
 
             if leaf_data_index < leaf_heights.len()
